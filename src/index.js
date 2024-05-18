6577e52c067c2a8e3674211a76f58fa005fb50ad
stsong0812 <stevensong0812@gmail.com>
2024-05-17 23:28:39 -0700

    Render fix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'; // Import createRoot from react-dom/client
 import './index.css';
 import App from './App';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-// Render the main App component into the root element
-ReactDOM.render(
+// Get the root element
+const rootElement = document.getElementById('root');
+
+// Create a root container
+const root = createRoot(rootElement);
+
+// Render the main App component into the root container
+root.render(
   <React.StrictMode> {/* Enable strict mode for highlighting potential problems in an application */}
     <App /> {/* Main App component */}
-  </React.StrictMode>,
-  document.getElementById('root') // The root element where the App component is rendered
+  </React.StrictMode>
 );
