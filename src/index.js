24c73a32dc7a91cf33aa766c552840035a064879
stsong0812 <stevensong0812@gmail.com>
2024-05-17 23:21:17 -0700

    Fixed index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'; // or any global styles
+import './index.css';
 import App from './App';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
+// Render the main App component into the root element
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode> {/* Enable strict mode for highlighting potential problems in an application */}
+    <App /> {/* Main App component */}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') // The root element where the App component is rendered
 );
